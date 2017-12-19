20adbb5ff637aee551f232348a0c5b8735998e20
hagmann.io <info@hagmann.io>
2017-12-19 14:24:09 +0100

    add login and uebung3

--- uebung3.php
+++ uebung3.php
@@ -5,165 +5,47 @@
 </head>
 <body>
 <?php
-// Array
-// Repetition: Key-Value erklären
-// http://php.net/manual/en/language.types.array.php
+//Funktionen
+
+function helloWorld()
+{
+    return 'Hello World';
+}
+//echo helloWorld();
 
 
-// Einfache Liste
-$pers = [
-    'Max',
-    'Julian',
-    'Jonas',
-    'Anna',
-    'Lisa'
-];
-
-// Eintrag hinzufügen
-$pers[] = 'Julia';
-
-// Eintrag mit Key entfernen
-unset($pers[0]);
-
-// Eintrag mit Value entfernen
-var_dump(array_search('Jonas', $pers));
-if (($key = array_search('Jonas', $pers)) !== false) {
-    unset($pers[$key]);
+function helloWorldWithParam($param)
+{
+    return 'Hello ' . $param;
 }
 
-// Letzten Eintrag entfernen
-array_pop($pers);
-
-// Array Keys zurücksetzen
-$pers = array_values($pers);
-var_dump($pers);
-
-// Liste generieren
-echo '<ul>';
-foreach ($pers as $p) {
-    echo '<li>' . $p . '</li>';
-}
-echo '</ul>';
+//echo helloWorldWithParam('World');
 
 
-// Array Inhalt anzeigen
-// Pre erklären
-echo '<pre>';
-var_dump($pers);
-echo '</pre>';
-
-
-// Array Key/Value
-$person = [
-    'Vorname' => 'Max',
-    'Nachname' => 'Muster'
-];
-
-// Neuer Array Eintrag
-$person[] = 123;
-
-// Strasse ergänzen
-$person['strasse'] = 'Musterstrasse 3';
-
-//var_dump($person);
-
-
-// Foreach
-echo '<ul>';
-foreach ($person as $p) {
-    echo '<li>' . $p . '</li>';
-}
-echo '</ul>';
-
-
-echo '<pre>';
-var_dump($person);
-echo '</pre>';
-
-
-// Foreach - Personenliste
-echo '<ul>';
-foreach ($person as $value => $key) {
-    echo '<li>' . $value . ': ' . $key . '</li>';
-}
-echo '</ul>';
-
-
-// Personenliste mit mehreren Einträgen
-
-$personen = [
-    [
-        'Vorname' => 'Max',
-        'Nachname' => 'Muster'
-    ],
-    [
-        'Vorname' => 'Julian',
-        'Nachname' => 'Koch'
-    ],
-    [
-        'Vorname' => 'Lisa',
-        'Nachname' => 'Bucher'
-    ]
-];
-
-
-//while($personen) {
-//    var_dump($personen);
-//}
-
-
-// Switch Anweisung
-
-$i = 2;
-
-switch ($i) {
-    case 0:
-        echo 'A ist 0' . '<br>';
-    case 1:
-        echo 'A ist 1' . '<br>';
-    case 2:
-        echo 'A ist 2' . '<br>';
+function addieren($a, $b)
+{
+    return $a + $b;
 }
 
+//echo addieren(3, 5);
 
-switch ($i) {
-    case 0:
-        echo 'A ist 0' . '<br>';
-        break;
-    case 1:
-        echo 'A ist 1' . '<br>';
-        break;
-    case 2:
-        echo 'A ist 2' . '<br>';
-        break;
-    default:
-        echo 'keine der Fälle trifft zu';
+
+function hallo($text = 'Welt')
+{
+    return 'Hallo ' . $text . '!';
 }
 
+//echo hallo();
 
-// For Schleife
-$i = 1;
-for ($i = 0; $i < 10; $i++) {
-    echo "Das ist Zeile $i<br>";
+
+
+// Date auf deutsch übersetzen (nur XAMPP)
+setlocale(LC_TIME, 'de_DE', 'deu_deu');
+function wasIstHeute() {
+    echo 'Heute ist ' . strftime('%A', mktime());
 }
-
-echo '<hr>';
-
-// Unterschied aufzeigen - Increment erst nach erstem Durchlauf
-$i = 1;
-while ($i < 10) {
-    echo "Das ist Zeile $i<br>";
-    $i++;
-}
-
-
-while ($i < 10):
-    echo "Das ist Zeile $i<br>";
-    $i++;
-endwhile;
-
+echo wasIstHeute();
 
 ?>
-
 </body>
 </html>
